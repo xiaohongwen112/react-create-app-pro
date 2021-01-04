140b0688d1658ee904445a23044f0f715821353e
xiaohongwen <965627565@qq.com>
2021-01-04 18:25:55 +0800

    增加路由

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React, {Component} from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css';
 import { Radio } from 'antd';
 import 'antd/dist/antd.css';
+import Admin from './pages/admin/admin'
+import Login from './pages/login/login'
 
 class App extends Component {
   state = {
@@ -20,20 +23,13 @@ class App extends Component {
           <Radio.Button value="default">Default</Radio.Button>
           <Radio.Button value="small">Small</Radio.Button>
         </Radio.Group>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/" component={Admin}></Route>
+          </Switch>
+        </BrowserRouter>
+
       </div>
     );
   }
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Admin extends Component{
+  render() {
+    return (
+      <div>
+      用户界面
+      </div>
+    )
+  }
+}
